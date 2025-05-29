47bc5093373310268c9ccc2b8d1da6f9e3869a41
AliShreim1 <ali.shreim@gmail.com>
2025-05-30 02:42:09 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, request, jsonify
+import torch
+from torchvision import models, transforms
+from PIL import Image
+import io
+import os
+
+app = Flask(__name__)
+
+
+model = models.mobilenet_v2(pretrained=False)
+model.classifier[1] = torch.nn.Linear(model.last_channel, 101)
+model.load_state_dict(torch.load('model.pth', map_location='cpu'))
+
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406],
+                         [0.229, 0.224, 0.225])
+])
+
+food101_classes = [
+    'apple_pie', 'baby_back_ribs', 'baklava', 'beef_carpaccio', 'beef_tartare',
+    'beet_salad', 'beignets', 'bibimbap', 'bread_pudding', 'breakfast_burrito',
+    'bruschetta', 'caesar_salad', 'cannoli', 'caprese_salad', 'carrot_cake',
+    'ceviche', 'cheesecake', 'cheese_plate', 'chicken_curry', 'chicken_quesadilla',
+    'chicken_wings', 'chocolate_cake', 'chocolate_mousse', 'churros', 'clam_chowder',
+    'club_sandwich', 'crab_cakes', 'creme_brulee', 'croque_madame', 'cup_cakes',
+    'deviled_eggs', 'donuts', 'dumplings', 'edamame', 'eggs_benedict',
+    'escargots', 'falafel', 'filet_mignon', 'fish_and_chips', 'foie_gras',
+    'french_fries', 'french_onion_soup', 'french_toast', 'fried_calamari', 'fried_rice',
+    'frozen_yogurt', 'garlic_bread', 'gnocchi', 'greek_salad', 'grilled_cheese_sandwich',
+    'grilled_salmon', 'guacamole', 'gyoza', 'hamburger', 'hot_and_sour_soup',
+    'hot_dog', 'huevos_rancheros', 'hummus', 'ice_cream', 'lasagna',
+    'lobster_bisque', 'lobster_roll_sandwich', 'macaroni_and_cheese', 'macarons', 'miso_soup',
+    'mussels', 'nachos', 'omelette', 'onion_rings', 'oysters',
+    'pad_thai', 'paella', 'pancakes', 'panna_cotta', 'peking_duck',
+    'pho', 'pizza', 'pork_chop', 'poutine', 'prime_rib',
+    'pulled_pork_sandwich', 'ramen', 'ravioli', 'red_velvet_cake', 'risotto',
+    'samosa', 'sashimi', 'scallops', 'seaweed_salad', 'shrimp_and_grits',
+    'spaghetti_bolognese', 'spaghetti_carbonara', 'spring_rolls', 'steak', 'strawberry_shortcake',
+    'sushi', 'tacos', 'takoyaki', 'tiramisu', 'tuna_tartare',
+    'waffles'
+]
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image file provided'}), 400
+    
+    file = request.files['image']
+    if file.filename == '':
+        return jsonify({'error': 'Empty filename'}), 400
+    
+    try:
+        img_bytes = file.read()
+        img = Image.open(io.BytesIO(img_bytes)).convert('RGB')
+        input_tensor = transform(img).unsqueeze(0)
+
+        with torch.no_grad():
+            outputs = model(input_tensor)
+            predicted_class_idx = outputs.argmax(1).item()
+            predicted_class_name = food101_classes[predicted_class_idx]
+
+        return jsonify({'predicted_class': predicted_class_name})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
